ebc576a44802c84d06df86dfd0bda4ad51bfa4f8
Marcelo Alarcón <marceloalarconbarrenechea@gmail.com>
2023-01-02 17:49:59 -0400

    Add js hamburger menu logic

---
+++ hamburguer.js
@@ -0,0 +1,24 @@
+let btnClose = document.querySelector('#btn-close');
+let divHamburguer = document.querySelector('.hamburguer');
+let iconHamburguer = document.querySelector('#hamburguer-icon');
+let hamburguer_link = document.querySelectorAll('.hamburguer-link');
+
+
+btnClose.addEventListener('click', function (event) {
+  divHamburguer.classList.add('run-out');
+  divHamburguer.classList.remove('run');
+});
+
+iconHamburguer.addEventListener('click', () => {
+  divHamburguer.classList.add('run');
+  divHamburguer.classList.remove('run-out');
+});
+
+for (let elem of hamburguer_link) {
+
+  elem.addEventListener('click', () => {
+    divHamburguer.classList.add('run-out');
+    divHamburguer.classList.remove('run');
+  })
+
+}
