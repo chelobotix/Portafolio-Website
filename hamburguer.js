56dcfc6cef28630183e4d6026994f4a3680234d9
Marcelo Alarcón <marceloalarconbarrenechea@gmail.com>
2023-01-02 21:53:20 -0400

    Fixed Hamburguer position problem

--- hamburguer.js
+++ hamburguer.js
@@ -1,24 +1,21 @@
-let btnClose = document.querySelector('#btn-close');
-let divHamburguer = document.querySelector('.hamburguer');
-let iconHamburguer = document.querySelector('#hamburguer-icon');
-let hamburguer_link = document.querySelectorAll('.hamburguer-link');
+const btnClose = document.querySelector('#btn-close');
+const divHamburguer = document.querySelector('.hamburguer');
+const iconHamburguer = document.querySelector('#hamburguer-icon');
+const hamburguerLink = document.querySelectorAll('.hamburguer-link');
 
-
-btnClose.addEventListener('click', function (event) {
-  divHamburguer.classList.add('run-out');
-  divHamburguer.classList.remove('run');
+btnClose.addEventListener('click', () => {
+  divHamburguer.classList.add('invisible');
+  divHamburguer.classList.remove('visible');
 });
 
 iconHamburguer.addEventListener('click', () => {
-  divHamburguer.classList.add('run');
-  divHamburguer.classList.remove('run-out');
+  divHamburguer.classList.add('visible');
+  divHamburguer.classList.remove('invisible');
 });
 
-for (let elem of hamburguer_link) {
-
-  elem.addEventListener('click', () => {
-    divHamburguer.classList.add('run-out');
-    divHamburguer.classList.remove('run');
-  })
-
+for (let i = 0; i < hamburguerLink.length; i += 1) {
+  hamburguerLink[i].addEventListener('click', () => {
+    divHamburguer.classList.add('invisible');
+    divHamburguer.classList.remove('visible');
+  });
 }
